5b93df575feba2543c77c56f059d8e1ff755d8d2
melanie-lei <65362046+melanie-lei@users.noreply.github.com>
2023-01-10 16:09:50 -0500

    servers set up kinda maybe? might not work

--- Client.java
+++ Client.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.HashMap;
@@ -24,7 +26,23 @@ public class Client implements Runnable{
     }
 
     public void localStart() throws Exception {
-        
+        System.out.println("Attempting to establish a connection ...");
+        clientSocket = new Socket(LOCAL_HOST, PORT);          //create and bind a socket, and request connection
+        output = new PrintWriter(clientSocket.getOutputStream());
+        input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        System.out.println("Connection to server established!");
+
+        //establish graphics panel
+        frame = new JFrame("GraphicsDemo");
+        panel = new GraphicsPanel();
+        frame.getContentPane().setPreferredSize(new Dimension(800, 600));
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel.setFocusable(true);
+        panel.requestFocus();
+        frame.add(panel);
+        frame.setVisible(true);
+        frame.setResizable(false);
     }
     
     public void stop() throws Exception {
--- Server.java
+++ Server.java
@@ -1,5 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Server {
-    public static void main(String[] args){
-        System.out.println(";alksjdfs");
+    final int PORT = 5000;
+
+    ServerSocket serverSocket;
+    Socket clientSocket;
+    int clientCounter = 0;
+
+    public static void main(String[] args) throws Exception{
+        Server server = new Server();
+        server.go();
+    }
+
+    public void go() throws Exception{
+        //create a socket with the local IP address and wait for connection request       
+        System.out.println("Waiting for a connection request from a client ...");
+        serverSocket = new ServerSocket(PORT);                //create and bind a socket
+        while(true) {
+            clientSocket = serverSocket.accept();             //wait for connection request
+            clientCounter = clientCounter +1;
+            System.out.println("Client "+clientCounter+" connected");
+            Thread connectionThread = new Thread(new ConnectionHandler(clientSocket));
+            connectionThread.start();                         //start a new thread to handle the connection
+
+
+        }
+    }
+
+    //------------------------------------------------------------------------------
+    class ConnectionHandler extends Thread {
+        Socket socket;
+        PrintWriter output;
+        BufferedReader input;
+        String data;
+        Queue<String> dataQ = new LinkedList<>();
+
+        public ConnectionHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        public void run() {
+
+            while (true) {
+                try {Thread.sleep(20);} catch (InterruptedException e) {throw new RuntimeException(e);}
+                try {
+                    //receive a message from the client
+                    if(input.ready()) {
+                        System.out.println("receiving data");
+                        data = input.readLine();
+                        dataQ.add(data);
+                    }
+                    output.flush();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
