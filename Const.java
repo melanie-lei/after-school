589ba3e07134b1ad1650732a0d03d7197864e706
Jaclyn Wang <53183074+yoeot@users.noreply.github.com>
2023-01-16 15:17:30 -0500

    constants

---
+++ Const.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+
+public final class Const {
+    public static final int WIDTH = (int) (Toolkit.getDefaultToolkit().getScreenSize().getWidth());
+    public static final int HEIGHT = (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight());
+
+}
